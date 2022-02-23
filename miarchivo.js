dc5002e18c14f8abf74b9510bc908ffc906fdca1
Gian Sbrissa <sbrissagf@gmail.com>
2022-02-23 15:51:20 -0300

    first commit

---
+++ miarchivo.js
@@ -0,0 +1,11 @@
+let numero1 = parseInt(prompt("Ingrese su edad"));
+let suma = 0;
+
+for (let i = 0; i < 1; i++) {
+
+    let numero2 = parseInt(prompt("Ingrese un número al azar"));
+    suma = numero1 + numero2;
+
+    console.log(suma);
+    alert("Su edad en " + numero2 + " años será de " + (suma));
+}
